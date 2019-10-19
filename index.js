147a103b72c1c461dae23f00733a0d0f5189d238
Donald Whitely <dswhitely1@gmail.com>
2019-10-19 13:11:23 -0400

    Reformatted Console.log

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./api/server');
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
-  console.log(`\n***Server Listening on PORT:${PORT}***\n`)
+  console.log(`\n*** Server Listening on PORT:${PORT} ***\n`)
 );
