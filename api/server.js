02aa9a788215d0c2a72975e81ef6ae1d35f0477c
Donald Whitely <dswhitely1@gmail.com>
2019-10-19 13:17:56 -0400

    Added Server middleware

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const middleware = require('./utils/middleware');
 
 const server = express();
+middleware(server);
 server.get('/', (req, res) => res.send(`It's Alive`));
 
 module.exports = server;
