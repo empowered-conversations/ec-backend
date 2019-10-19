1e3f89cfba334c31e17ccc2cc2eeddf43da736ac
Donald Whitely <dswhitely1@gmail.com>
2019-10-19 13:58:38 -0400

    Created Users Migration

---
+++ data/migrations/20191019135610_create_users_table.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', tbl => {
+    tbl.increments();
+    tbl
+      .string('username', 128)
+      .notNullable()
+      .unique();
+    tbl.string('password', 128).notNullable();
+    tbl.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
