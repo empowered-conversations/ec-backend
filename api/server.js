ada8de37572facfadee71b00121d2244ad60fe6c
Donald Whitely <dswhitely1@gmail.com>
2019-10-19 13:27:50 -0400

    Bring in routes

--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const middleware = require('./utils/middleware');
+const routes = require('./routes/routes.index');
 
 const server = express();
 middleware(server);
-server.get('/', (req, res) => res.send(`It's Alive`));
-
+routes(server);
 module.exports = server;
