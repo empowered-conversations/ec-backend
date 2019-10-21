0eb0421e954c52214e52f5fbd7eb00a779d8b9e2
Justin Trombley <jtrombley96@gmail.com>
2019-10-21 17:08:50 -0400

    Register and Logins functions

--- knexfile.js
+++ knexfile.js
@@ -1,39 +1,36 @@
 // Update with your config settings.
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      host: process.env.MYDBHOST,
-      database: process.env.MYDBNAME,
-      user: process.env.MYDBPERSON,
-      password: process.env.MYDBPASSWORD,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-  },
+	development : {
+		client           : 'sqlite3',
+		connection       : {
+			filename : './data/db.db3',
+		},
+		pool             : {
+			min : 2,
+			max : 10,
+		},
+		useNullAsDefault : true,
+		migrations       : {
+			directory : './data/migrations',
+		},
+		seeds            : {
+			directory : './data/seeds',
+		},
+	},
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-  },
+	production  : {
+		client     : 'pg',
+		connection : process.env.DATABASE_URL,
+		pool       : {
+			min : 2,
+			max : 10,
+		},
+		migrations : {
+			directory : './data/migrations',
+		},
+		seeds      : {
+			directory : './data/seeds',
+		},
+	},
 };
