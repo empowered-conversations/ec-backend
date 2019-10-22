145b1239e31f2f565794d1c664f9b53c627e7c4d
Justin Trombley <jtrombley96@gmail.com>
2019-10-21 20:45:07 -0400

    Migration Texts

--- api/routes/routes.index.js
+++ api/routes/routes.index.js
@@ -1,7 +1,9 @@
 const testRoute = require('./test/test.routes');
 const authRoutes = require('./auth/auth.routes');
+const textRoutes = require('./text/text.routes');
 
 module.exports = server => {
   server.use('/', testRoute);
   server.use('/api/auth', authRoutes);
+  server.use('/text', textRoutes);
 };
--- data/migrations/20191019135610_create_users_table.js
+++ data/migrations/20191019135610_create_users_table.js
@@ -1,5 +1,7 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('users', tbl => {
+  return knex.schema
+  
+    .createTable('users', tbl => {
     tbl.increments();
     tbl
       .string('username', 128)
@@ -7,9 +9,17 @@ exports.up = function(knex) {
       .unique();
     tbl.string('password', 128).notNullable();
     tbl.timestamps(true, true);
-  });
+  })
+  .createTable('texts', tbl => {
+    tbl.increments();
+    tbl.string("friend's name", 40).notNullable();
+    tbl.integer("friend's number", 11).notNullable()
+    tbl.string("your name", 40).notNullable()
+    tbl.integer("your number", 11).notNullable()
+  })
+
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('users');
+  return knex.schema.dropTableIfExists('texts').dropTableIfExists('users')
 };
