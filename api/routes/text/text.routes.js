346a42b121ab1483b5fdd0b4cc3b561e525268d8
Justin Trombley <jtrombley96@gmail.com>
2019-10-23 17:17:33 -0400

    Bug Fix

--- api/routes/text/text.routes.js
+++ api/routes/text/text.routes.js
@@ -4,29 +4,29 @@ const Texts = require('../../../data/models/texts.model');
 function getTexts (req, res) {
     Texts.findAll()
     .then(texts => res.json(texts))
-    .catch(err => res.status(500).json('getTexts', err));
+    .catch(err => res.status(500).json(err));
 }
 
 textRouter.get("/:id", (req, res) => {
         Texts.findBy(req.params.id)
         .then(text => res.json(text))
-        .catch(err => res.status('I am a string').json('getSpecificTexts',err))
+        .catch(err => res.status('I am a string').json(err))
 })
 
 function addText (req, res) {
     Texts.add(req.body)
         .then(newText => res.status(201).json(newText[0]))
-        .catch(err => res.status(500).json('addText',err))
+        .catch(err => res.status(500).json(err))
 }
 function updateText(req, res) {
     Texts.update(req.params.id, req.body)
     .then(updated)
-    .catch(err => res.status(500).json('updateText', err))
+    .catch(err => res.status(500).json(err))
 }
 function deleteText(req, res) {
     Texts.remove(req.params.id)
         .then(text => res.json(text))
-        .catch(err => res.status(500).json('deleteText',err))
+        .catch(err => res.status(500).json(err))
 }
 
 textRouter.get('/', getTexts).post('/', addText).put('/:id', updateText).delete('/:id', deleteText)
